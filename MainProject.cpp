092fd7e1154c90b61fba789927c5259200e55db0
WesE-zip <westhevans@gmail.com>
2024-11-21 13:44:11 -0500

    5

--- MainProject.cpp
+++ MainProject.cpp
@@ -14,12 +14,26 @@ string ReadMsg(string thing){
     return inp;
 }
 
+string separate(string inp){
+    string outp;
+
+    for(int i = 0;i < inp.length();i++){
+        if(isalpha(inp[i])){
+            outp.push_back(inp[i]);
+        }
+    }
+
+    return outp;
+}
 
 int main() {
-    string word;
+    string word, cut;
     word = ReadMsg("string");
 
+    cut = separate(word);
+
     cout << word <<"\n";
+    cout << cut << "\n";
 
 
     return 0;
