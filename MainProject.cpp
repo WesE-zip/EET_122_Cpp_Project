105cbf72df46d50e96345e69ecb075388f382552
WesE-zip <westhevans@gmail.com>
2024-11-25 15:34:30 -0500

    8

--- MainProject.cpp
+++ MainProject.cpp
@@ -3,25 +3,11 @@
 #include <string>
 using namespace std;
 
-string ReadMsg(string thing){
-    string inp;
 
-    cout << "Input the " << thing << ": ";
+string word, cut, encripted, inpToDcr, cutDcr, DCRed;
+int CesarShiftnum;
 
-    getline(cin, inp);
-  
-
-    return inp;
-}
-
-int GetNum(string thing){
-    int num;
-
-    cout << "Input the " << thing << ": ";
-
-    cin >> num;
-    return num;
-}
+const string alph = "abcdefghijklmnopqrstuvwxyz";
 
 string separate(string inp){
     string outp;
@@ -35,22 +21,66 @@ string separate(string inp){
     return outp;
 }
 
-string CeasarEncr(string inp, int shift){
+string ReadThing(string thing){
+    string inp;
+
+    cout << "Input the " << thing << ": ";
+    cin.ignore();
+    getline(cin, inp);
+
+  
+
+    return inp;
+}
+
+void ReadPlain(){
+    cout << "Input the Plaintext: ";
+    cin.ignore();
+    getline(cin, word);
+    cut = separate(word);    
+}
+
+void ReadCypher(){
+    cout << "Input the Cyphertext: ";
+    cin.ignore();
+    getline(cin, inpToDcr);
+    cutDcr = separate(inpToDcr);    
+}
+
+int GetNum(string thing){
+    int num;
+
+    cout << "Input the " << thing << ": ";
+
+    cin >> num;
+    return num;
+}
+
+void encrOutp(string data){
+    cout << "The Plaintext is: " << word << endl<< data << endl << "The Cyphertext is: " << encripted << endl;
+}
+
+void dcrOutp(string data){
+    cout << "The Cyphertext is: " << inpToDcr << endl<< data << endl << "The decripted Plaintext is: " << DCRed << endl;
+}
+
+
+void CeasarEncr(){
     string outp;
     bool test = false;
-    int shiftnum;
+    int shift;
     
-    string alph = "abcdefghijklmnopqrstuvwxyz";
+    
 
-    for(int i = 0;i < inp.length();i++){
+    for(int i = 0;i < cut.length();i++){
         for(int l = 0; l < alph.length();l++){
-            if(inp[i] == alph[l]){
-                if((l + shift) < 26){
-                    shiftnum = l + shift;
+            if(cut[i] == alph[l]){
+                if((l + CesarShiftnum) < 26){
+                    shift = l + CesarShiftnum;
                 } else {
-                    shiftnum = (l + shift) - 26;
+                    shift = (l + CesarShiftnum) - 26;
                 }
-                outp.push_back(alph[shiftnum]);
+                outp.push_back(alph[shift]);
                 test = true;
             }
         }
@@ -58,55 +88,69 @@ string CeasarEncr(string inp, int shift){
     }
 
 
-    return outp;
+    encripted = outp;
 }
 
-string CeasarDcr(string inp, int shift){
+void CeasarDcr(){
     string outp;
     bool test = false;
-    int shiftnum;
+    int shift;
     
-    string alph = "abcdefghijklmnopqrstuvwxyz";
 
-    for(int i = 0;i < inp.length();i++){
+    for(int i = 0;i < cutDcr.length();i++){
         for(int l = 0; l < alph.length();l++){
-            if(inp[i] == alph[l]){
-                if((l - shift) > 0){
-                    shiftnum = l - shift;
+            if(cutDcr[i] == alph[l]){
+                if((l - CesarShiftnum) > 0){
+                    shift = l - CesarShiftnum;
                 } else {
-                    shiftnum = (l - shift) + 26;
+                    shift = (l - CesarShiftnum) + 26;
                 }
-                outp.push_back(alph[shiftnum]);
+                outp.push_back(alph[shift]);
                 test = true;
             }
         }
         test = false;
     }
 
-
-    return outp;
+    DCRed = outp;
+    
 }
+
+void runCeasar(){
+    int t;
+    string sh = "The shiftnum is: ";
+    cout << "Input shiftnum: ";
+    cin >> CesarShiftnum;
+    sh += to_string(CesarShiftnum);
+    while(t != 0){
+        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n";
+        cin >> t;
+        if(t == 1){
+            ReadPlain();
+            CeasarEncr();
+            encrOutp(sh);
+        }else if(t == 2){
+            ReadCypher();
+            CeasarDcr();
+            dcrOutp(sh);
+        }else if(t != 0){
+            cout << "ERROR" << endl;
+        }
+    }
+
+    cout << "End Ceaser Cypher" << endl;
+}
+
+
+/*void run(){
+
+}*/
 
 int main() {
-    string word, cut;
-    int num;
-    word = ReadMsg("string");
-
-    cut = separate(word);
-
-    cout << word <<"\n";
-    cout << cut << "\n";
-
-    cout << "to encript press 1 to decript press 2 ";
-    cin >> num;
-
-    if(num == 1){
-        cout << CeasarEncr(cut, GetNum("number of the shift must be < 26")) << endl;
-    }else {
-        cout << CeasarDcr(cut, GetNum("number of the shift must be < 26")) << endl;
-    }
-
     
+    runCeasar();
+
+
 
 
 
