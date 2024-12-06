d60379a83aad26b1b1163556279df7dbbe18c6dc
WesE-zip <westhevans@gmail.com>
2024-12-06 10:01:55 -0500

    13

--- MainProject.cpp
+++ MainProject.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 
-string word, cut, encripted, inpToDcr, cutDcr, DCRed;
-string Vkey, VkeyT, Akey;
+string word, cut, encripted, inpToDcr, cutDcr, DCRed; //Variable declaration of the main variables
+string Vkey, VkeyT, Akey;//Declaration of variables for the encription methods
 int CesarShiftnum;
-int Lnums[200];
 
-const string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Declaration of an alphabet string
 
+//Function to clear the main variables
 void clear() {
     word = "";
     cut = "";
@@ -20,6 +20,7 @@ void clear() {
     DCRed = "";
 }
 
+//Function to separate out everything but letters and make them uppercase
 string separate(string inp){
     string outp;
     
@@ -32,6 +33,7 @@ string separate(string inp){
     return outp;
 }
 
+/*
 string ReadThing(string thing){
     string inp;
 
@@ -41,7 +43,9 @@ string ReadThing(string thing){
 
     return inp;
 }
+*/
 
+//Reads the plaintext and assingns the Uppercase letter only form to the variable cut
 void ReadPlain(){
     cout << "Input the Plaintext: ";
     cin.ignore();
@@ -49,6 +53,7 @@ void ReadPlain(){
     cut = separate(word);    
 }
 
+//Reads the Cyphertext and assingns the Uppercase letter only form to the variable cutDcr
 void ReadCypher(){
     cout << "Input the Cyphertext: ";
     cin.ignore();
@@ -56,44 +61,35 @@ void ReadCypher(){
     cutDcr = separate(inpToDcr);    
 }
 
-void toNum(string inp) {
-
-}
-
-int GetNum(string thing){
-    int num;
-
-    cout << "Input the " << thing << ": ";
-
-    cin >> num;
-    return num;
-}
-
+//Prints the encription output
 void encrOutp(string data){
     cout << "The Plaintext is: " << word << endl<< data << endl << "The Cyphertext is: " << encripted << endl;
 }
 
+//Prints the decription output
 void dcrOutp(string data){
     cout << "The Cyphertext is: " << inpToDcr << endl<< data << endl << "The decripted Plaintext is: " << DCRed << endl;
 }
 
 
+//Encripts using Ceasar Cypher
 void CeasarEncr(){
+    //Variable declaration
     string outp;
     bool test = false;
     int shift;
     
     
 
-    for(int i = 0;i < cut.length();i++){
-        for(int l = 0; l < alph.length();l++){
+    for(int i = 0;i < cut.length();i++){ //Goes through the plain text
+        for(int l = 0; l < alph.length();l++){ //Goes through the alphabet for each letter
             if(cut[i] == alph[l]){
-                if((l + CesarShiftnum) < 26){
+                if((l + CesarShiftnum) < 26){ //gives the positon of the encriped char making sure to not go out of bounds
                     shift = l + CesarShiftnum;
                 } else {
                     shift = (l + CesarShiftnum) - 26;
                 }
-                outp.push_back(alph[shift]);
+                outp.push_back(alph[shift]); //Pushes back each char to the output
                 test = true;
             }
         }
@@ -104,21 +100,23 @@ void CeasarEncr(){
     encripted = outp;
 }
 
+//Decripts using Ceaser Cypher
 void CeasarDcr(){
+    //Variable declaration
     string outp;
     bool test = false;
     int shift;
     
 
-    for(int i = 0;i < cutDcr.length();i++){
-        for(int l = 0; l < alph.length();l++){
+    for(int i = 0;i < cutDcr.length();i++){ //Goes through the Cyphertext
+        for(int l = 0; l < alph.length();l++){ //Goes through the alphabet for each letter
             if(cutDcr[i] == alph[l]){
-                if((l - CesarShiftnum) >= 0){
+                if((l - CesarShiftnum) >= 0){ //gives the positon of the encriped char making sure to not go out of bounds
                     shift = l - CesarShiftnum;
                 } else {
                     shift = (l - CesarShiftnum) + 26;
                 }
-                outp.push_back(alph[shift]);
+                outp.push_back(alph[shift]); //Pushes back each char to the output
                 test = true;
             }
         }
@@ -129,38 +127,41 @@ void CeasarDcr(){
     
 }
 
+//Runs the Ceaser Cypher menu
 void runCeasar(){
-    int t;
+    int t; //Input num
     string sh = "The shiftnum is: ";
-    cout << "Input shiftnum: ";
+    cout << "Input shiftnum: ";//reading the shiftnum
     cin >> CesarShiftnum;
-    sh += to_string(CesarShiftnum);
+    sh += to_string(CesarShiftnum); //Preaparing the shiftnum output
     while(t != 0){
-        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n";
+        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n"; //Menu prompt and t read
         cin >> t;
-        if(t == 1){
-            ReadPlain();
-            CeasarEncr();
-            encrOutp(sh);
-        }else if(t == 2){
-            ReadCypher();
-            CeasarDcr();
-            dcrOutp(sh);
+        if(t == 1){ //runs encription
+            ReadPlain(); //reads the plaintext
+            CeasarEncr(); //Encripts with Ceaser cypher
+            encrOutp(sh); //outputs
+        }else if(t == 2){ //runs decription
+            ReadCypher(); //reads the cyphertext
+            CeasarDcr(); //decripts
+            dcrOutp(sh); //outputs
         }else if(t != 0){
-            cout << "ERROR" << endl;
+            cout << "ERROR" << endl; //In case they input sompthing else
         }
     }
 
     cout << "End Ceaser Cypher" << endl;
-    clear();
+    clear(); //Clears in out vars
 }
 
+//makes a key for vigenere cypher
 void makeKeyV(int t) {
+    //variable declaration
     int L;
     int KL = VkeyT.length();
     string K = VkeyT;
 
-    if(t == 1){
+    if(t == 1){//so encription or decription can come first
         L = cut.length();
     }
     if(t == 2){
@@ -168,7 +169,7 @@ void makeKeyV(int t) {
     }
 
 
-    while(L >= K.length()){
+    while(L >= K.length()){ //Repeats the keyword and adds it
         for(int i = 0; i < KL && K.length() <= L; i++) {
             K.push_back(VkeyT[i]);
         }
@@ -177,13 +178,19 @@ void makeKeyV(int t) {
     Vkey = K;
 }
 
+//Encripts with vigenere cypher
 void VigenereEncr(){
+    //variable declaration
     string outp;
     int a, b, t, f;
-    for(int i = 0; i < cut.length(); i++) {
+
+    for(int i = 0; i < cut.length(); i++) { //Loops through plaintext
+        //finds the positon of the chars in the alphabet
         a = alph.find(cut[i]);
         b = alph.find(Vkey[i]);
+        //Does the math to encript
         t = (a + b) % 26;
+        //makes sure there is no out of bounds
         if((t) < 26){
             if(t < 0){
                 f = t + 26;
@@ -198,13 +205,19 @@ void VigenereEncr(){
     encripted = outp;
 }
 
+//Decripts with Vigenere Cypher
 void VigenereDcr(){
+    //Variable declaration
     string outp;
     int a, b, t, f;
-    for(int i = 0; i < cutDcr.length(); i++) {
+
+    for(int i = 0; i < cutDcr.length(); i++) { //loops through cyphertext
+        //finds the positon of the chars in the alphabet
         a = alph.find(cutDcr[i]);
         b = alph.find(Vkey[i]);
+        //Does the math to decript
         t = (a - b) % 26;
+        //makes sure it is not going out of bounds
         if((t) < 26){
             if(t < 0){
                 f = t + 26;
@@ -219,25 +232,26 @@ void VigenereDcr(){
     DCRed = outp;
 }
 
+//Runs Vigenere cypher menu
 void runVigenere() {
-    int t;
-    cout << "Input the key: ";
+    int t; //Input num
+    cout << "Input the key: ";//keyword input
     cin >> VkeyT;
 
     while(t != 0){
-        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n";
+        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n"; //Menu
         cin >> t;
-        if(t == 1){
-            ReadPlain();
-            makeKeyV(t);
-            VigenereEncr();
-            encrOutp("The key is: " + VkeyT);
-        }else if(t == 2){
-            ReadCypher();
-            makeKeyV(t);
-            VigenereDcr();
-            dcrOutp("The key is: " + VkeyT);
-        }else if(t != 0){
+        if(t == 1){ //Encription
+            ReadPlain(); //Reads plaintext
+            makeKeyV(t); //makes the key for vigenere cypher
+            VigenereEncr(); //Encripts 
+            encrOutp("The key is: " + VkeyT); //Outputs
+        }else if(t == 2){ //Decription
+            ReadCypher(); //Reads Cyphertext
+            makeKeyV(t); //makes the key for viginere cypher
+            VigenereDcr(); //Decripts
+            dcrOutp("The key is: " + VkeyT); //Outputs
+        }else if(t != 0){ //Incase they input sompthing else
             cout << "ERROR" << endl;
         }
     }
@@ -246,46 +260,42 @@ void runVigenere() {
     clear();
 }
 
-void makeKeyA(int t) {
+//Makes an autokey which is a different type of key for Vigenere
+void makeKeyA() {
+    //variable declaration
     int L;
     string K = Akey;
     string msg;
+    msg = cut;
 
-    if(t == 1){
-        L = cut.length();
-        msg = cut;
-    }
-    if(t == 2){
-        L = cutDcr.length();
-        msg = cutDcr;
-    }
-
-    K += msg.substr(0, (msg.length() - 2));
+    K += msg.substr(0, (msg.length() - 2)); //Makes the key which is the key letter with the plaintext added onto it minus the last letter
 
     Vkey = K;
 }
 
+//Runs the autokey cypher menu
 void runAutokey() {
-    int t;
-    cout << "Input the key: ";
+    int t; //Input num
+    cout << "Input the key: "; //Gets the key letter
     cin >> Akey;
-    ReadPlain();
-    makeKeyA(t);
-    VigenereEncr();
-    encrOutp("The key is: " + Akey);
+    //Starts right into encription since it must come first
+    ReadPlain(); //Reads plaintext
+    makeKeyA(); //Makes autokey
+    VigenereEncr(); //Uses the Vigenere Encription to encript
+    encrOutp("The key is: " + Akey); //Outputs
     while(t != 0){
-        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n";
+        cout << "Press 0 to exit\nPress 1 to encript\nPress 2 to decript \n"; //Menu 
         cin >> t;
-        if(t == 1){
-            ReadPlain();
-            makeKeyA(t);
-            VigenereEncr();
-            encrOutp("The key is: " + Akey);
-        }else if(t == 2){
-            ReadCypher();
-            VigenereDcr();
-            dcrOutp("The key is: " + Akey);
-        }else if(t != 0){
+        if(t == 1){ //Encription
+            ReadPlain(); //Reads plaintext
+            makeKeyA(); //Makes autokey
+            VigenereEncr(); //Uses the Vigenere Encription to encript
+            encrOutp("The key is: " + Akey); //Outputs
+        }else if(t == 2){ //Decription
+            ReadCypher(); //Reads Cyphertext
+            VigenereDcr(); //Uses the Vigenere Decription to Decript
+            dcrOutp("The key is: " + Akey); //Outputs
+        }else if(t != 0){ //Incase they input sompthing else
             cout << "ERROR" << endl;
         }
     }
@@ -294,21 +304,22 @@ void runAutokey() {
     clear();
 }
 
+//Runs the main menu
 void run(){
-    int t;
+    int t; //Input num
 
 
     while(t != 0){
-        cout << "Press 1 for Ceaser Cypher \nPress 2 for Vigenere Cypher \nPress 3 for Autokey Cypher \nPress 0 to exit" << endl;
+        cout << "Press 1 for Ceaser Cypher \nPress 2 for Vigenere Cypher \nPress 3 for Autokey Cypher \nPress 0 to exit" << endl; //menu text
         cin >> t;
         cin.ignore();
-        if(t == 1){
+        if(t == 1){ //Runs Ceaser Cypher menu
             runCeasar();
-        }else if(t == 2) {
+        }else if(t == 2) { //Runs Vigenere menu
             runVigenere();
-        }else if(t == 3){
+        }else if(t == 3){ //Runs Autokey menu
             runAutokey();
-        }else if(t != 0){
+        }else if(t != 0){ //Incase they input sompthing else
             cout << "ERROR" << endl;
         }
     }
@@ -316,7 +327,7 @@ void run(){
 
 int main() {
     
-    run();
+    run(); //Runs main menu
 
 
 
